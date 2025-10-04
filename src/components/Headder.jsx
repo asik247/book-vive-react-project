b1a33f5fcb4e5efdf5a48c6933913aa153d76c91
Asikur Rahman <mdasikur5893@gmail.com>
2025-10-05 00:03:02 +0600

    hero are added

---
+++ src/components/Headder.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import img from "../assets/books.jpg"
+
+const Headder = () => {
+  return (
+    <div className="hero bg-base-200 min-h-[50vh]">
+      <div className="hero-content flex-col md:flex-row-reverse">
+        <img
+          src={img}
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Headder;
--- src/pakge/Main.jsx
+++ src/pakge/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Headder from '../components/Headder';
 
 const Main = () => {
     return (
         <div className='min-h-[calc(100vh-287px)] container'>
+           <Headder></Headder>
            <h1>hi</h1>
         </div>
     );
